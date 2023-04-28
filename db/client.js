c07bec3692cd448550f7fbf12bbaa33d2de47854
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2023-04-28 10:54:39 -0400

    adds check for production for ssl

--- db/client.js
+++ db/client.js
@@ -1,13 +1,15 @@
 const { Client } = require('pg');
 const { DATABASE_URL } = process.env;
+console.log(process.env);
 
 const connectionString = DATABASE_URL || 'postgres://localhost:5432/our_pets';
 
 const client = new Client({
   connectionString,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 module.exports = { client };
